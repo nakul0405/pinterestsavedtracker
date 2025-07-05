225004d6a81e7319b50c55a8d07cb17dee22f4a5
Nakul Rathod <142690696+nakul0405@users.noreply.github.com>
2025-07-06 01:49:36 +0530

    Update store.py

--- store.py
+++ store.py
@@ -1,27 +1,22 @@
-import json, os
+import os, json
 
 def _load():
     if not os.path.exists("users.json"):
-        with open("users.json", "w") as f: json.dump({}, f)
-    with open("users.json", "r") as f:
-        return json.load(f)
+        json.dump({}, open("users.json","w"))
+    return json.load(open("users.json"))
 
-def _save(data):
-    with open("users.json", "w") as f:
-        json.dump(data, f, indent=2)
+def _save(u):
+    json.dump(u, open("users.json","w"), indent=2)
 
-def save_user_cookie(user_id, cookie):
-    data = _load()
-    if str(user_id) not in data: data[str(user_id)] = {}
-    data[str(user_id)]["cookie"] = cookie
-    _save(data)
+def save_user_cookie(uid, cookie):
+    u = _load()
+    u.setdefault(str(uid), {})["cookie"] = cookie
+    _save(u)
 
-def save_target_user(user_id, username):
-    data = _load()
-    if str(user_id) not in data: data[str(user_id)] = {}
-    data[str(user_id)]["username"] = username
-    _save(data)
+def save_target_user(uid, username):
+    u = _load()
+    u.setdefault(str(uid), {})["username"] = username
+    _save(u)
 
-def get_user_data(user_id):
-    data = _load()
-    return data.get(str(user_id))
+def get_user_data(uid):
+    return _load().get(str(uid))
